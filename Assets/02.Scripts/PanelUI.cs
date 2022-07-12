2e39cc15e09ac573af0cbd7650e8a6a53fbcfa31
SimGyuYeong <shgy5901@gmail.com>
2022-07-12 15:40:07 +0900

    벌 정보 UI 제작

--- Assets/02.Scripts/PanelUI.cs
+++ Assets/02.Scripts/PanelUI.cs
@@ -10,6 +10,7 @@ public class PanelUI : MonoBehaviour
     private TextMeshProUGUI _levelText;
     private TextMeshProUGUI _priceText;
     private TextMeshProUGUI _damageText;
+    private Image _buttonImage;
     private Image _icon;
     private int _slot;
 
@@ -21,6 +22,7 @@ public class PanelUI : MonoBehaviour
         _levelText = transform.Find("level").GetComponent<TextMeshProUGUI>();
         _priceText = transform.Find("Button/price").GetComponent<TextMeshProUGUI>();
         _damageText = transform.Find("damage").GetComponent<TextMeshProUGUI>();
+        _buttonImage = transform.Find("Button").GetComponent<Image>();
         _icon = transform.Find("Icon").GetComponent<Image>();
 
         _shop = transform.parent.parent.parent.GetComponent<Shop>();
@@ -29,11 +31,13 @@ public class PanelUI : MonoBehaviour
     public void Refresh(Bee bee, BeeUpgrade upgrade)
     {
         _nameText.text = bee.beeName;
-        _levelText.text = bee.level.ToString();
-        _priceText.text = upgrade.price.ToString();
+        _levelText.text = string.Format($"Lv.{bee.level}");
+        _priceText.text = upgrade.GetPrice().ToString();
         _damageText.text = bee.damage.ToString();
         _icon.sprite = bee.icon.sprite;
         _slot = upgrade.slot;
+        
+        _buttonImage.color = upgrade.IsPurchase() ? Color.white : Color.gray;
     }
 
     public void Purchase()
